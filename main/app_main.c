b5cab70ccd28311c06fccd17aece204def32203a
rondindon <ronnie.rybarik@gmail.com>
2023-11-13 22:17:36 +0100

    first commit

---
+++ main/app_main.c
@@ -0,0 +1,241 @@
+/* MQTT (over TCP) Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+
+#include "stdio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+#include "time.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+
+#define led  2
+
+static const char *TAG = "MQTT_EXAMPLE";
+
+#define    FIRST            15
+#define    SECOND           4
+#define    THIRD            5
+#define    FOURTH           18
+#define    FIFTH            19
+
+void resetLeds() {
+                gpio_set_level(FIRST,0);
+            gpio_set_level(SECOND,0);
+            gpio_set_level(THIRD,0);
+            gpio_set_level(FOURTH,0);
+            gpio_set_level(FIFTH,0);
+}
+
+void board_reset(){
+    gpio_reset_pin(FIRST);
+    gpio_reset_pin(SECOND);
+    gpio_reset_pin(THIRD);
+    gpio_reset_pin(FOURTH);
+    gpio_reset_pin(FIFTH);
+
+    gpio_set_direction(FIRST, GPIO_MODE_OUTPUT);
+    gpio_set_direction(SECOND, GPIO_MODE_OUTPUT);
+    gpio_set_direction(THIRD, GPIO_MODE_OUTPUT);
+    gpio_set_direction(FOURTH, GPIO_MODE_OUTPUT);
+    gpio_set_direction(FIFTH, GPIO_MODE_OUTPUT);
+}
+
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0) {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
+}
+
+/*
+ * @brief Event handler registered to receive MQTT events
+ *
+ *  This function is called by the MQTT client event loop.
+ *
+ * @param handler_args user data registered to the event.
+ * @param base Event base for the handler(always MQTT Base in this example).
+ * @param event_id The id for the received event.
+ * @param event_data The data for the event, esp_mqtt_event_handle_t.
+ */
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        // ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        // msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+        // ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+
+        // msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+        // ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        // msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+        // ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        // msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+        // ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        msg_id = esp_mqtt_client_publish(client, "/ronnie/test", "data_rr", 0, 1, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "/ronnie/test", 0);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED");
+        msg_id = esp_mqtt_client_publish(client, "/ronnie/test", "data", 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+
+
+        if(strncmp(event->data, "on", event->data_len) == 0){
+            gpio_set_level(led,1);
+        }
+        if(strncmp(event->data, "off", event->data_len) == 0){
+            gpio_set_level(led,0);
+        }
+
+        if(strncmp(event->data, "0", event->data_len) == 0){
+            resetLeds();
+        }  
+
+        if(strncmp(event->data, "1", event->data_len) == 0){
+            resetLeds();
+            gpio_set_level(FIRST,1);
+        }
+        if(strncmp(event->data, "2", event->data_len) == 0){
+            resetLeds();
+            gpio_set_level(FIRST,1);
+            gpio_set_level(SECOND,1);
+        }
+        if(strncmp(event->data, "3", event->data_len) == 0){
+            resetLeds();
+           gpio_set_level(FIRST,1);
+           gpio_set_level(SECOND,1);
+           gpio_set_level(THIRD,1);
+        }
+        if(strncmp(event->data, "4", event->data_len) == 0){
+            resetLeds();
+            gpio_set_level(FIRST,1);
+            gpio_set_level(SECOND,1);
+            gpio_set_level(THIRD,1);
+            gpio_set_level(FOURTH,1);
+        }
+        if(strncmp(event->data, "5", event->data_len) == 0){
+            resetLeds();
+            gpio_set_level(FIRST,1);
+            gpio_set_level(SECOND,1);
+            gpio_set_level(THIRD,1);
+            gpio_set_level(FOURTH,1);
+            gpio_set_level(FIFTH,1);
+        }
+
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+
+        }
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+static void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = CONFIG_BROKER_URL,
+    };
+
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+}
+
+void app_main(void)
+{
+    board_reset();
+    gpio_reset_pin(led);
+gpio_set_direction(led, GPIO_MODE_OUTPUT);
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
+    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
+    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+    esp_log_level_set("outbox", ESP_LOG_VERBOSE);
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    mqtt_app_start();
+}
